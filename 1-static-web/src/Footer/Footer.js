43e83231938b33d0ad6fc9d7efa6ea4b1bbde878
kxnyshk <ikanishk1172@gmail.com>
2022-05-06 01:25:00 +0530

    first static react.js web app craeted

---
+++ 1-static-web/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './Footer.css';
+
+function Footer(){
+    return(
+      <footer className="footer">
+          <small>© kxnyshk dev. 2022 | All rights reserved</small>
+      </footer>
+    )
+}
+
+export default Footer
